6f13943b6a48021f0b7688ff16f561bfe209a22f
Rome Bell <romebell5@gmail.com>
2020-07-20 20:02:25 -0400

    [js] Add final notes

--- app.js
+++ app.js
@@ -138,3 +138,66 @@ headerThree.addEventListener('click', function() {
 // 1. friend 1
 // 2. friend 2
 // 3. friend 3
+
+// Problem Solving
+
+// what do I start with
+// what do I hope to end up with
+
+// understand the problem
+// where the issue start and ...
+
+// break it down into more manageable problems
+
+
+// fizzBuzz
+// Write a function that does the following:
+    // takes in an array
+    // check each number in the array
+    // if the number is divisible 5 and 3 === FizzBuzz
+    // if the number is divisible by 3 === Fizz
+    // if the number is divisible by 5 === 'Buzz'
+// return an array
+
+// For example:
+// [3, 5, 15, 20, 9, 7]
+// ['Fizz', 'Buzz', 'FizzBuzz', 'Buzz', 'Fizz', 7]
+
+// I need to set up an empty
+// iterate through the array that going to be passed in
+// check the remainder of each element
+// push the result of each element inside the result array
+// result that result array
+
+function fizzBuzz(array) {
+    let result = []; // Fizz, Buzz, FizzBuzz, Buzz, Fizz, 7
+
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        console.log(num); // 3, 5, 15,  ....
+
+        if (num % 3 === 0 && num % 5 === 0) {
+            result.push('FizzBuzz');
+        } else if (num % 3 === 0) {
+            result.push('Fizz');
+        } else if (num % 5 === 0) {
+            result.push('Buzz');
+        } else {
+            result.push(num);
+        }
+    }
+
+    return result;
+}
+
+console.log(fizzBuzz([3, 5, 15, 20, 9, 7]));
+
+// [ ] Do I understand what the prompt is asking?
+// [ ] Am I able to break the problem down?
+// [ ] What is my strategy for solving the problem?
+// [ ] Do I understand what my code is doing?
+// [ ] Am I able to foresee any edge cases?
+// [ ] Am I able to track the order of the code that is being executed?
+// [ ] Am I able to track the values of the variables?
+// [ ] Am I able to walk through my code line by line with an example?
+// [ ] Do I have a working solution?
